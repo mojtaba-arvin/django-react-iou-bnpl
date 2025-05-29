ba6c2d6364cc8911be9d0f4fde999dcf7eaf957f
Mojtaba Arvin <m.arvin@live.com>
2025-05-28 20:08:16 -0400

    chore(settings): add default value for credit score to simulate credit scores in DEBUG mode

--- backend/bnpl/settings/development.py
+++ backend/bnpl/settings/development.py
@@ -18,3 +18,9 @@ CORS_ALLOW_ALL_ORIGINS = True
 # Enable Swagger UI only in development
 SWAGGER_ENABLED = True
 SWAGGER_API_URL = config('SWAGGER_API_URL', default='http://localhost:8000')
+
+# Default credit score assigned to new customer profiles in DEBUG mode only.
+# This value is used *only* during development and testing for simulated credit checks.
+# Do NOT import this setting directly in your app code.
+# Instead, always import `CREDIT_SCORE_DEFAULT_DEBUG` from `customers/constants.py`.
+CREDIT_SCORE_DEBUG_DEFAULT_VALUE = config('CREDIT_SCORE_DEBUG_DEFAULT_VALUE', default=600, cast=int)
