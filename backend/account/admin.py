2c4f6d1e99e2278f57722b055d796cefbf69990f
Mojtaba Arvin <m.arvin@live.com>
2025-05-29 04:52:11 -0400

    fix(admin): replace deprecated get_inlines method with get_inline_instances to ensures compatibility with future Django versions

--- backend/account/admin.py
+++ backend/account/admin.py
@@ -1,9 +1,10 @@
-from typing import List, Optional, Type
+from typing import List, Optional
 
 from django.contrib import admin
+from django.contrib.admin.options import InlineModelAdmin
+from django.http import HttpRequest
 from django.contrib.auth import get_user_model
 from django.contrib.auth.admin import UserAdmin as DjangoUserAdmin
-from django.contrib.admin import StackedInline
 
 from customer.admin import CustomerProfileInline
 from merchant.admin import MerchantProfileInline
@@ -33,6 +34,7 @@ class CustomUserAdmin(DjangoUserAdmin):
             },
         ),
     )
+    list_editable = ('is_active',)
     add_fieldsets = (
         (
             None,
@@ -55,26 +57,26 @@ class CustomUserAdmin(DjangoUserAdmin):
     search_fields = ('email',)
     ordering = ('email',)
 
-    def get_inlines(
-        self, request: admin.options.Request, obj: Optional[User] = None
-    ) -> List[Type[StackedInline]]:
+    def get_inline_instances(
+        self, request: HttpRequest, obj: Optional[User] = None
+    ) -> List[InlineModelAdmin]:
         """
-        Return the appropriate inline(s) based on the user's type.
+        Return the appropriate inline instance(s) based on the user's type.
 
         Args:
-            request (Request): The current admin request instance.
+            request (HttpRequest): The current admin request instance.
             obj (Optional[User]): The user object being edited.
 
         Returns:
-            List[Type[StackedInline]]: A list of inlines to display in the admin.
+            List[InlineModelAdmin]: A list of instantiated inline admin classes to display in the admin.
         """
         if not obj:
             return []
 
         if obj.user_type == User.UserType.CUSTOMER:
-            return [CustomerProfileInline]
+            return [CustomerProfileInline(self.model, self.admin_site)]
         if obj.user_type == User.UserType.MERCHANT:
-            return [MerchantProfileInline]
+            return [MerchantProfileInline(self.model, self.admin_site)]
         return []
 
     add_fieldsets = DjangoUserAdmin.add_fieldsets
